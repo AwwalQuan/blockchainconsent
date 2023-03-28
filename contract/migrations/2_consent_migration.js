088ba54474f1aa70d982207ec5aa0ab8fb6f4f16
Awwal Ishiaku <48957591+AwwalQuan@users.noreply.github.com>
2023-03-28 08:35:13 +0300

    Update 2_consent_migration.js

--- contract/migrations/2_consent_migration.js
+++ contract/migrations/2_consent_migration.js
@@ -7,7 +7,7 @@ module.exports = function(deployer) {
     var surgeryDate='24-March-2024';
     var risks='None';
     var patientAddress='0x52CfF12eae83154d665011E493B144265E0385BE';
-    var surgeonIdentity='Rufai';
-    var validityPeriod='60';
+    var surgeonIdentity='0x76f0961247eF40D4Ccc71aDC3F6ab3a56c36A44f';
+    var validityPeriod='60000';
     deployer.deploy(Consent, patientName, surgeryType, surgeryDate, risks, patientAddress, surgeonIdentity, validityPeriod);
 };
