155d57f0b305cb4aa0f36574954d6966baf46457
Awwal Ishiaku <48957591+AwwalQuan@users.noreply.github.com>
2023-03-28 08:52:46 +0300

    Update and rename contract.py to slave_robot.py

--- slave_robot.py
+++ slave_robot.py
@@ -1,6 +1,10 @@
 from web3 import Web3
+from eth_account.messages import encode_defunct
+from web3.auto import w3
 import datetime
 import json
+import socket
+import uuid
 import colorama
 from colorama import Fore
 
@@ -8,11 +12,15 @@ from colorama import Fore
 ganache_url = "http://127.0.0.1:7545"
 web3 = Web3(Web3.HTTPProvider(ganache_url))
 
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+
 # Replace with your address
 user_address = "0x52CfF12eae83154d665011E493B144265E0385BE"
 private_key = "0x15f75d6216ae540382e63bd64cb4844f4d552783a7e3af255fada68dacb7361b"
 user_contracts = []
 surgeon_identity = ""
+remote_control = False # Variable to verify of remote control has been granted to master console or not
 
 #The ABI is saved in a file named abi.json
 with open('abi.json', 'r') as f:
@@ -47,6 +55,49 @@ def extract_surgeon_identity(contract_address):
     print("Contract Details:", contract_details)
     return surgeon_identity
 
+def sign_challenge(challenge, private_key):
+    # Sign the challenge message with the private key
+    message = encode_defunct(text=challenge)
+    signed_message = w3.eth.account.sign_message(message, private_key=private_key)
+    return signed_message.signature.hex()
+
+def authenticate_surgeon(conn, surgeon_address):
+    # Generate a random challenge and send it to the client
+    nonce = nonce = uuid.uuid4().hex
+    challenge = "Please sign this challenge: " + nonce
+    #print(challenge)
+    conn.sendall(challenge.encode())
+
+    # Receive the signed challenge from the client
+    signature = conn.recv(1024).decode()
+    
+    # Verify the signature using the client's public address
+    message = encode_defunct(text=challenge)
+    verified_address = w3.eth.account.recover_message(message, signature=signature)
+    
+    if verified_address.lower() == surgeon_address.lower():
+        print(Fore.GREEN + '\033[1m' + 'Surgeon authentication successful.' + '\033[0m')
+        # Grant remote control to the master console
+        remote_control = True
+        conn.sendall("Authentication successful.\n".encode())
+        print(Fore.GREEN + '\033[1m' + 'Remote control granted to the master console.\nAll authentication steps have been completed, and the surgery is about to begin...'  + '\033[0m')
+        conn.sendall("Remote control granted.".encode())
+        # Some function to handle the rest of the surgical process
+    else:
+        print(Fore.RED + '\033[1m' + 'Surgeon authentication failed.'  + '\033[0m')
+        conn.sendall("Authentication failed.".encode())
+    
+def open_socket():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        print('Server listening on', (HOST, PORT))
+        while True:
+            conn, addr = s.accept()
+            with conn:
+                print('Client connected:', addr)
+                # Authenticate the surgeon with public address
+                authenticate_surgeon(conn, surgeon_identity)
 
 # Authenticate account
 account = web3.eth.account.from_key(private_key)
@@ -64,6 +115,8 @@ if account.address.lower() == user_address.lower():
             print("Surgeon Identity: " + surgeon_identity)
             # open socket to listen to connection from the master console
             # Authenticate the master console
+            print("Waiting for the surgeon to connect and authenticate...")
+            open_socket()
         else:
             print(Fore.RED + '\033[1m' + 'Invalid contract selected' + '\033[0m')
     except ValueError:
